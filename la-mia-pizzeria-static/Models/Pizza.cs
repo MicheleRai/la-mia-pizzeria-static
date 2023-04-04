71bf7d456db533e96c9faadbe40b64c016391662
MicheleRai <rainonemichele1999@gmail.com>
2023-04-04 14:32:52 +0200

    Add project files.

---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,20 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Foto { get; set; }
+        public double Prezzo { get; set; }
+
+        public Pizza() { }
+
+        public Pizza(string name, string description, string foto, double prezzo)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Foto = foto;
+            this.Prezzo = prezzo;
+        }
+    }
+}
